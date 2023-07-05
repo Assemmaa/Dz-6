2fd43e51d79efd4b908f5ffe2b5801c926169c64
Assemmaa <asemaibraeva88@gmail.com>
2023-07-05 23:10:37 +0600

    first commit

---
+++ src/redux/productsSlice.js
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit";
+import prod from './products.json'
+
+const initialState = {
+    products: prod,
+}
+
+
+
+const productsSlice = createSlice({
+    name: 'productsSlice',
+    initialState,
+    reducers: {}
+})
+
+
+
+export default productsSlice.reducer;
+export const productsSelect = state => state.productsSlice;
